d864b14cf8a3e835fc160a7ada9c4014fd7bfbe7
deepsky <2739985700@qq.com>
2021-11-06 19:22:33 +0800

    citel task1

--- day01/src/banking/Main.java
+++ day01/src/banking/Main.java
@@ -8,28 +8,18 @@ package banking;
 
 //import banking.*;
 
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
-import java.util.Objects;
 import java.util.Scanner;
 
-public class Main{
-
-    public static String formatTosepara(float data) {
-        DecimalFormat df = new DecimalFormat("#,###.00");
-        return df.format(data);
-    }
-
+class Main {
     public static void main(String[] args) {
         Bank bank = new Bank();
-        NumberFormat currency_format = NumberFormat.getCurrencyInstance();
-
         Customer customer;
+        Account account;
         int curCustomer = 0;
         Scanner s = new Scanner(System.in);
         int t = s.nextInt();
         s.nextLine();
-// Create several customers and their accounts according to data
+        // Create several customers and their accounts according to data
         while (t-- > 0) {
             String f = s.next();
             String l = s.next();
@@ -41,58 +31,66 @@ public class Main{
             while (numAccount-- > 0) {
                 String[] type = s.nextLine().split(" ");
                 double balance;
-                double interesOrProtect;
-                if (Objects.equals(type[0], "C") || Objects.equals(type[0], "c")) {
+                boolean protectWithSavings;
+                char op = type[0].charAt(0);
+                if (op == 'C' || op == 'c') {
                     balance = Double.parseDouble(type[1]);
-                    if (type.length == 3) {
-                        interesOrProtect = Double.parseDouble(type[2]);
-                        customer.addAccount(new CheckingAccount(balance,
-                                interesOrProtect));
+                    protectWithSavings = Boolean.parseBoolean(type[2]);
+                    if (protectWithSavings) {
+                        customer.setChecking(new CheckingAccount(balance,
+                                customer.getSavings().balance));
+                        System.out.println(customer + " create Checking account: " + customer.getChecking().getBalance() + " with a protecting savings accout: " + customer.getSavings().getBalance() );
                     } else {
-                        customer.addAccount(new CheckingAccount(balance));
-
+                        customer.setChecking(new CheckingAccount(balance));
+                        System.out.println(customer + " create Checking account: " + customer.getChecking().getBalance() + " without protecting savings accout.");
                     }
-                } else if (Objects.equals(type[0], "S") || Objects.equals(type[0], "s")) {
+                } else if (op == 'S' || op == 's') {
                     balance = Double.parseDouble(type[1]);
-                    interesOrProtect = Double.parseDouble(type[2]);
-                    customer.addAccount(new SavingsAccount(balance,
-                            interesOrProtect));
-                } else if (Objects.equals(type[0], "A") || Objects.equals(type[0], "a")) {
-                    int cIndex = Integer.parseInt(type[1]);
-                    int aIndex = Integer.parseInt(type[2]);
-                    customer.addAccount(bank.getCustomer(cIndex).getAccount(
-                            aIndex));
+                    double interestRate = Double.parseDouble(type[2]);
+                    customer.setSavings(new SavingsAccount(balance,
+                            interestRate));
+                    System.out.println(customer + " create savings account: " + customer.getSavings().getBalance() );
                 }
             }
         }
-
-// Generate a report
-        System.out.println("CUSTOMERS REPORT");
-        System.out.println("================");
-
-        for ( int cust_idx = 0; cust_idx < bank.getNumOfCustomers(); cust_idx++ ) {
-            customer = bank.getCustomer(cust_idx);
-
-            System.out.println();
-            System.out.println("Customer: "
-                    + customer.getLastName() + ", "
-                    + customer.getFirstName());
-
-            for ( int acct_idx = 0; acct_idx < customer.getNumOfAccounts(); acct_idx++ ) {
-                Account account = customer.getAccount(acct_idx);
-                String account_type = "";
-                if(account instanceof SavingsAccount){
-                    System.out.print("    Savings Account$");
-                    System.out.println(formatTosepara((float) account.getBalance()));
-                }
-                else{
-                    System.out.print("    Checking Account$");
-                    System.out.println(formatTosepara((float) account.getBalance()));
-                }
+        int nOPs = s.nextInt();
+        s.nextLine();
+        while (nOPs-- > 0) {
+            String[] sOP = s.nextLine().split(" ");
+            char op = sOP[0].charAt(0);
+            int customerIndex = Integer.parseInt(sOP[1]);
+            double amount = Double.parseDouble(sOP[2]);
+            boolean result;
+            switch(op){
+                case 'w':
+                case 'W':
+                    customer = bank.getCustomer(customerIndex);
+                    result = customer.getChecking().withdraw(amount);
+                    System.out.println("Checking acct " + customer + "withdraw " + amount + " succeeds? " + result);
+                    break;
+                case 'd':
+                case 'D':
+                    customer = bank.getCustomer(customerIndex);
+                    result = customer.getChecking().deposit(amount);
+                    System.out.println("Checking acct " + customer + "deposit " + amount + " succeeds? " + result);
+                    break;
             }
+        }
+        // Generate a report
+        for (int cust_idx = 0; cust_idx < bank.getNumOfCustomers(); cust_idx++) {
+            customer = bank.getCustomer(cust_idx);
+            System.out.println();
+            System.out.println("Customer: " + customer.getLastName() + ", "
+                    + customer.getFirstName());
+            account = customer.getChecking();
+            System.out.println("    has a checking balance of " + account.getBalance());
+            account = customer.getSavings();
+            if (account != null)
+                System.out.println("    has a savings balance of " + account.getBalance());
         }
     }
 }
+/* PRESET CODE END - NEVER TOUCH CODE ABOVE */
 
 
 
@@ -117,12 +115,17 @@ class Bank{
 class Customer{
     private String firstName;
     private String lastName;
-    private Account[] accounts;
-    private int number;
+//    private Account savingsAccount;
+    private CheckingAccount checkingAccount;
+
+    @Override
+    public String toString() {
+        return "["+lastName+", "+firstName+"]";
+    }
+
     public Customer(String f,String l){
         firstName=f;
         lastName=l;
-        accounts=new Account[20];
     }
 
     public String getFirstName() {
@@ -133,20 +136,22 @@ class Customer{
         return lastName;
     }
 
-    public void addAccount(Account account){
-        accounts[number++]=account;
+    public Account getSavings(){
+        return checkingAccount.getSavingAccount();
     }
 
-    public Account getAccount(int index){
-        return accounts[index];
+    public Account getChecking(){
+        return checkingAccount;
+    }
+    public void setSavings(Account account){
+        checkingAccount.setSavingAccount(account);
+    }
+    public void setChecking(Account account){
+        checkingAccount= (CheckingAccount) account;
     }
 
-    public int getNumOfAccounts(){
-        return number;
-    }
 
 }
-
 
 class Account {
     protected double balance;
@@ -177,28 +182,42 @@ class SavingsAccount extends Account{
         super(balance);
         this.interestRate=interest_rate;
     }
+
+    public SavingsAccount(double protection) {
+        super(protection);
+    }
 }
 
+
 class CheckingAccount extends Account{
-    private double overdraftProtection;
+
+    private Account savingAccount;
+
     public CheckingAccount(double balance){
         super(balance);
     }
     public CheckingAccount(double balance,double protection){
         super(balance);
-        this.overdraftProtection=protection;
+        savingAccount=new SavingsAccount(protection);
     }
     public boolean withdraw(double amt){
         if(balance>=amt){
             balance-=amt;
             return true;
         }
-        else if(overdraftProtection+balance>=amt){
-            overdraftProtection-=amt-balance;
+        else if(savingAccount.balance+balance>=amt){
+            savingAccount.balance-=amt-balance;
             balance=0;
             return true;
         }
         return false;
     }
 
+    public void setSavingAccount(Account savingAccount) {
+        this.savingAccount = savingAccount;
+    }
+
+    public Account getSavingAccount() {
+        return savingAccount;
+    }
 }
